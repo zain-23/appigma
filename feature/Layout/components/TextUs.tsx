c8937d00d5a187e7f667a0c011ac4bb9363b486e
zain-23 <zaingit780@gmail.com>
2025-10-21 10:12:11 +0500

    fix: update TextUs component visibility for improved layout on larger screens

--- feature/Layout/components/TextUs.tsx
+++ feature/Layout/components/TextUs.tsx
@@ -4,7 +4,7 @@ export const TextUs = () => {
   return (
     <Link
       href={"/contact-us"}
-      className="fixed lg:top-1/2 top-150 left-0 -translate-y-1/2 -translate-x-9 -rotate-90 origin-center bg-primary text-xl px-4 py-2 z-50 font-semibold rounded-b-3xl"
+      className="hidden lg:block fixed top-1/2 left-0 -translate-y-1/2 -translate-x-9 -rotate-90 origin-center bg-primary text-xl px-4 py-2 z-50 font-semibold rounded-b-3xl"
     >
       Text Us
     </Link>
